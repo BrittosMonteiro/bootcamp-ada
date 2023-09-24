2a9313bdcbf97fb68c5af7b4fd5c43ad65a3b3ef
Lucas <brittosmonteiro@gmail.com>
2023-09-24 01:20:05 -0300

    Work on dark and light theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
-import Image from 'next/image'
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { setTheme } = useTheme();
+
   return (
-    <main className="flex min-h-screen flex-col items-start gap-[24px]">
-    </main>
-  )
+    <main className="flex min-h-screen flex-col items-start gap-[24px]"></main>
+  );
 }
